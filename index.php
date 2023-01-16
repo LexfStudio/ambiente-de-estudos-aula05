5d7eecc7d2800d479cfe392ec4da65d03b8821e2
LexfStudio <lexfstudio@gmail.com>
2023-01-15 21:49:21 -0300

    aula 05

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php require_once('app\paths.php'); ?>
+<?php $title = 'LexfStudio | Home' ?>
+
+<?php include_once(PATH_HEADER) ?>
+
+<!-- Conteúdo principal das páginas: Begin -->
+<main class="main">
+    <h1>Lexf Studio - Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, necessitatibus officia cum amet ratione doloremque impedit? Obcaecati est iure dignissimos id ut inventore libero nihil, ipsum quasi, consequatur laboriosam ullam?</p>
+    <br><br>
+</main>
+<!-- Conteúdo principal das páginas: End -->
+
+<?php include_once(PATH_FOOTER) ?>
